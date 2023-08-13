5fcccc085304d3a20b2200d3ddfa5a0948e9486d
Debora <debora.pc@gmail.com>
2023-08-13 15:46:38 -0300

    Criado CRUD APi REST

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const crypto = require("crypto");
+
+const app = express();
+
+app.use(express.json());
+
+const db = [];
+const procuraId = (req, res, next) => {
+  const id = req.params.id;
+
+  req.index = db.findIndex((item) => item.id === id);
+
+  if (req.index == -1) {
+    return res.status(404).json({ error: "item not found." });
+  }
+
+  next();
+};
+
+// Lista todos
+app.get("/api", (_, res) => {
+  res.json(db);
+});
+
+// Create
+app.post("/api", (req, res) => {
+  const id = crypto.randomUUID();
+  db.push({ id, ...req.body });
+  res.status(201).json({ id });
+});
+
+// Read
+app.get("/pai/:id", procuraId, (req, res) => {
+  res.json(db[req.index]);
+});
+
+// Update
+app.put("/pai/:id", procuraId, (req, res) => {
+  db[req.index] = { ...db[req.index], ...req.body };
+  res.json(db[req.index]);
+});
+
+// Delete
+app.delete("/pai/:id", procuraId, (req, res) => {
+  db.splice(req.index, 1);
+  res.sendStatus(204);
+});
+
+// Menssagem de status servidor
+const status = "Servidor rodando em http://localhost:3000";
+
+//iniciar o Servidor
+app.listen(3000, () => console.log(status));
+
+// Execute a aplicação Api Rest
